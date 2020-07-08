2d9c783a34cee8c98cb45dfe471c8cc20be8d688
Taylor McKeel <josephmckeel1993@gmail.com>
2020-07-08 19:00:32 -0400

    class

--- actions.js
+++ actions.js
@@ -1,7 +1,33 @@
-const axios = require('axios');
+const axios = require("axios");
 
 //TYPES
-const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
+const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require("./types");
 
 /* YOUR CODE GOES HERE */
+
+const gotDesserts = (des) => {
+  return {
+    type: GOT_DESSERTS_DATA,
+    des,
+  };
+};
+
+const addDessert = (des) => {
+  return {
+    type: NEW_DESSERT_DATA,
+    des,
+  };
+};
+
+const fetchDesserts = () => {};
+
+const postDessert = () => {};
+
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
+
+const getChefs = () => {
+  return async (dispatch) => {
+    const chefs = await axios.get("/api/chefs").data;
+    return dispatch(setChefs(chefs));
+  };
+};
